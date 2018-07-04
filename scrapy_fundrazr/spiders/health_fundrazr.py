9db85014f984684f3526710ce0254cf8358ccb6c
Uroš Jevremović <jevremovic.uros91@gmail.com>
2018-07-04 13:14:51 +0200

    fixed a bug that was causing acquiring text from a story field to stop when it encountered <strong> or any other tag

--- scrapy_fundrazr/spiders/health_fundrazr.py
+++ scrapy_fundrazr/spiders/health_fundrazr.py
@@ -20,26 +20,29 @@ class HealthFundrazrSpider(scrapy.Spider):
             yield scrapy.Request(url=next_page_url, callback=self.parse)
 
     def parse_details(self, response):
-        check=response.xpath("//div[contains(@class, 'stats-primary with-goal')]//span[contains(@class, "
+        check = response.xpath("//div[contains(@class, 'stats-primary with-goal')]//span[contains(@class, "
                              "'stats-label hidden-phone')]/text()")
+
         if check:
 
             yield {
-                # 'title': response.xpath("//div[contains(@id, 'campaign-title')]/descendant::text()").extract_first().strip(),
-                'title': response.css('div.headline-font::text').extract_first().strip(),
+                'title': response.xpath("//div[contains(@id, 'campaign-title')]/descendant::text()").extract_first().strip(),
                 'amount_raised': response.xpath("//span[contains(@class,'stat')]/span[contains(@class, 'amount-raised')]/descendant::text()").extract_first().strip() + ' ' + response.xpath("//div[contains(@class, 'stats-primary with-goal')]/@title").extract_first(),
                 'goal': response.xpath("//div[contains(@class, 'stats-primary with-goal')]//span[contains(@class, 'stats-label hidden-phone')]/text()").extract()[1].strip()[3:][:-5],
                 'number_of_contributors': response.xpath("//div[contains(@class, 'stats-secondary with-goal')]//span[contains(@class, 'donation-count stat')]/text()").extract_first(),
-                'story': response.xpath("//div[contains(@id, 'full-story')]/descendant::text()").extract_first().strip()
+                'story': response.xpath("//div[contains(@id, 'full-story')]//p//text()").extract(),
             }
 
         else:
 
             yield {
-                # 'title': response.xpath("//div[contains(@id, 'campaign-title')]/descendant::text()").extract_first().strip(),
-                'title': response.css('div.headline-font::text').extract_first().strip(),
+                'title': response.xpath("//div[contains(@id, 'campaign-title')]/descendant::text()").extract_first().strip(),
                 'amount_raised': response.xpath("//span[contains(@class,'stat')]/span[contains(@class, 'amount-raised')]/descendant::text()").extract_first().strip() + ' ' + response.xpath("//div[contains(@class, 'stats-primary')]/@title").extract_first(),
                 'goal': 'no goal amount set',
                 'number_of_contributors': response.xpath("//div[contains(@class, 'stats-secondary with-goal')]//span[contains(@class, 'donation-count stat')]/text()").extract_first(),
-                'story': response.xpath("//div[contains(@id, 'full-story')]/descendant::text()").extract_first().strip()
+                'story': response.xpath("//div[contains(@id, 'full-story')]//p//text()").extract(),
             }
+
+
+# div.quote > span > a::attr(href)
+# 'h3.author-title::text'
